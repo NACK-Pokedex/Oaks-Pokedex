b1186f35288d0191e4a2eb32d86ecee66149ce12
Anabel-Santillan <anabels.20022@gmail.com>
2022-08-05 14:27:16 -0400

    Server.js added

---
+++ server.js
@@ -0,0 +1,9 @@
+const app = require('./index');
+const { sequelize } = require('./db');
+
+const { PORT = 3000 } = process.env;
+
+app.listen(PORT, () => {
+  sequelize.sync({ force: false });
+  console.log(`Pokedex is ready at http://localhost:${PORT}`);
+});
