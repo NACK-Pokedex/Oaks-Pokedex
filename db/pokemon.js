4002e1856139305a4ed41ff761dc2479681b56f8
nicolasd1323 <nicolasd1323@gmail.com>
2022-08-12 15:23:16 -0400

    seed Data

--- db/pokemon.js
+++ db/pokemon.js
@@ -4,7 +4,6 @@ const Pokemon = sequelize.define('pokemon', {
   num: Sequelize.STRING,
   name: Sequelize.STRING,
   img: Sequelize.STRING,
-  // type: Sequelize.ARRAY(Sequelize.TEXT),
   height: Sequelize.STRING,
   weight: Sequelize.STRING,
   candy: Sequelize.STRING,
@@ -12,22 +11,7 @@ const Pokemon = sequelize.define('pokemon', {
   egg: Sequelize.STRING,
   spawn_chance: Sequelize.BIGINT,
   avg_spawns: Sequelize.BIGINT,
-  spawn_time: Sequelize.STRING,
-  // multipliers: Sequelize.ARRAY(Sequelize.BIGINT),
-  // weaknesses: Sequelize.ARRAY(Sequelize.TEXT),
-  // prev_evolution: Sequelize.ARRAY( {
-  //   num: Sequelize.STRING,
-  //   name: Sequelize.STRING,
-  // },{
-  //   num: Sequelize.STRING,
-  //   name: Sequelize.STRING,
-  // }),
-  // prev_evolution: {
-  //   type: Sequelize.STRING,
-  //   set(val){
-  //     this.setDataValue("prev_evolution", JSON.stringify(val ?? ""))
-  //   }
-  // }, 
+  spawn_time: Sequelize.STRING, 
   next_evolution: {
     type: Sequelize.STRING,
     set(val){
